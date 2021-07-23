2066dd17dea56111b5e5cddf900c2889d01815c7
Felipe Baptista <felipe.baptista06@gmail.com>
2021-07-23 17:51:10 -0300

    Finaliza

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const { json } = require("express");
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const todoRoute = require("./routes/todo");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(express.json())
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import Item from "./Components/item";
 
 function App() {
+  const [itens, setItens] = useState([]);
+  const [filterItens, setFilterItens] = useState({ filter: false, active: true });
+
+  function getData() {
+    fetch('http://localhost:3000/todo/list', { method: "GET" })
+      .then(response => response.json())
+      .then(data => setItens(data))
+  }
+
+  function insertDocument() {
+    fetch("http://localhost:3000/todo/add", { method: "POST", headers: { 'Content-Type': "application/json" }, body: JSON.stringify({ "text": "", "edit": false, "active": true }) })
+      .then(response => response.json())
+      .then(() => getData());
+  }
+
+  function updateDocument(item) {
+    fetch("http://localhost:3000/todo/update", { method: "PATCH", headers: { 'Content-Type': "application/json" }, body: JSON.stringify(item) })
+      .then(response => response.json())
+      .then(() => getData());
+  }
+
+  function deleteDocument(item) {
+    fetch("http://localhost:3000/todo/delete", { method: "DELETE", headers: { 'Content-Type': "application/json" }, body: JSON.stringify(item) })
+      .then(response => response.json())
+      .then(() => getData());
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const itensToShow = filterItens.filter ? itens.filter(item => item.active === filterItens.active) : itens
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="todo-list">
+        <h1>To Do App</h1>
+
+        {itensToShow.map(item => {
+          return <Item item={item} updateDocument={updateDocument} deleteDocument={deleteDocument} />
+        })}
+
+        <div className="button-row">
+          <button
+            onClick={() => setFilterItens({ filter: false })}
+            style={filterItens.filter ? {} : { fontWeight: "bold" }}
+          >Todos</button>
+          <button
+            onClick={() => setFilterItens({ filter: true, active: true })}
+            style={(filterItens.filter && filterItens.active === true) ? { fontWeight: "bold" } : {}}
+          >Pendentes</button>
+          <button
+            onClick={() => setFilterItens({ filter: true, active: false })}
+            style={(filterItens.filter && filterItens.active === false) ? { fontWeight: "bold" } : {}}
+          >Concluídos</button>
+        </div>
+
+        <div className="button-row">
+          <button onClick={insertDocument}>Inserir novo To-do</button>
+        </div>
+
+      </div >
     </div>
+
   );
 }
 
